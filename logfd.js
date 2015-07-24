10dd9282cdca824c3b2184f6f2cccac679d8bc36
Bacra <bacra.woo@gmail.com>
2015-07-24 20:55:55 +0800

    Add: EventEmitter

--- logfd.js
+++ logfd.js
@@ -1,7 +1,8 @@
-var QPD		= require('qpd').QPD;
-var xtend	= require('xtend');
+var QPD			= require('qpd').QPD;
+var events		= require('events');
+var extend		= require('extend');
+var debug		= require('debug')('logfd');
 var dateformat	= require('dateformat');
-var debug	= require('debug')('logfd');
 
 
 exports = module.exports = main;
@@ -11,7 +12,7 @@ exports.defaults = {
 };
 
 function Logfd(opts) {
-	this.opts = xtend({}, exports.defaults, opts);
+	this.opts = extend({}, exports.defaults, opts);
 	if (this.opts.prefdTime < 0) {
 		this.opts.prefdTime = 0;
 	}
@@ -19,11 +20,14 @@ function Logfd(opts) {
 	// 罗列成员变量
 	this._deadline = 0;
 	this.file = this.qpd = this.preQpd = null;
+	events.EventEmitter.call(this);
 
 	this.init_();
 }
 
-Logfd.prototype = {
+require('util').inherits(Logfd, events.EventEmitter);
+
+extend(Logfd.prototype, {
 	init_: function() {
 		var now = new Date;
 		var qpd = this._genQPD(now);
@@ -83,6 +87,7 @@ Logfd.prototype = {
 		self.file	= qpd._logfdFile;
 		var next	= self._nextTime(now);
 		self._deadline = +next;
+		self.emit('switch', qpd, oldQpd, now);
 
 		// 提前准备fd
 		if (self.opts.prefdTime) {
@@ -96,6 +101,7 @@ Logfd.prototype = {
 
 				var newQpdTime = new Date(self._deadline+1);
 				self.preQpd = self._genQPD(newQpdTime);
+				self.emit('preQpd', self.preQpd);
 
 			}, self._deadline - now - self.opts.prefdTime);
 		}
@@ -105,7 +111,7 @@ Logfd.prototype = {
 		now = +now;
 		return new Date(now - now % 3600000 + 3600000);
 	}
-};
+});
 
 
 function main(opts) {
